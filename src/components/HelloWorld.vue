127bf1788e13cf8b59c19da61bfb8be0fd229c9b
hotdogc1017 <hotdogc1017@gmail.com>
2025-01-10 11:09:46 +0800

    feat: Update demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
@@ -10,9 +11,9 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">
+    <Button type="button" @click="count++">
       count is {{ count }}
-    </button>
+    </Button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
