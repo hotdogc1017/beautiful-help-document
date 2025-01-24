201d351bf8a0bbeadcf7384edaa058ab81342c92
hotdogc1017 <hotdogc1017@gmail.com>
2025-01-24 14:46:29 +0800

    feat: add multiple cli options

--- proxy-server/cli.ts
+++ proxy-server/cli.ts
@@ -1,16 +1,35 @@
-#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --unstable
+#!/usr/bin/env -S deno run --ext=js --allow-env --allow-net --allow-read --allow-write --unstable
 
+import { parseArgs } from '@std/cli'
 import { streamConvertInputToMarkdownByAI } from './utils/markdown.ts'
 
-const decoder = new TextDecoder()
-for await (const chunk of Deno.stdin.readable) {
-  const input = decoder.decode(chunk)
+const args = parseArgs(Deno.args, {
+  string: ['prompt'],
+  boolean: ['help'],
+  alias: {
+    prompt: 'p',
+    help: 'h',
+  },
+})
 
-  const { textStream, text } = streamConvertInputToMarkdownByAI(input, {
-    prompt: `将以下内容翻译为中文, 记住, 你的回答只需要给我结果。${input}`,
-  })
+const helpMessage = `Usage: cli.ts [options]
+--help,-h Output help information
+--prompt,-p Message sent to the large language model`
 
-  textStream
-    .pipeThrough(new TextEncoderStream())
-    .pipeTo(Deno.stdout.writable)
+if (args.help) {
+  console.log(helpMessage)
+}
+else {
+  const decoder = new TextDecoder()
+  for await (const chunk of Deno.stdin.readable) {
+    const input = decoder.decode(chunk)
+
+    const { textStream } = streamConvertInputToMarkdownByAI(input, {
+      prompt: args.prompt ?? `将以下内容翻译为中文, 记住, 你的回答只需要给我结果。${input}`,
+    })
+
+    textStream
+      .pipeThrough(new TextEncoderStream())
+      .pipeTo(Deno.stdout.writable)
+  }
 }
